31f5da1fe5eb73df39f9a658f65cc99428f35600
kkh <sbs554342@naver.com>
2022-04-19 14:37:12 +0900

    scanExam 수정

--- 0419/src/scanExam.java
+++ 0419/src/scanExam.java
@@ -7,13 +7,11 @@ public class scanExam {
 		
 		System.out.print("1000 이하의 자연수를 입력하세요 : ");
 		int a=sc.nextInt();
-		int b=0;
 		int sum=0;
 		
 		for(int i=1;i*4<a; i++) {
-			b=i*4;
-			System.out.println(b);
-			sum+=b;
+			System.out.println(i*4);
+			sum+=i*4;
 		}
 		System.out.println("1~"+a+"까지의 4의 배수의 합은 " + sum + "입니다.");
 
